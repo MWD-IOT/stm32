d037dbbaee23498c69cb89b8e7d3c6cecff7a2e4
moweida <2594041017@qq.com>
2024-08-05 19:33:16 +0800

    add some files

---
+++ STDLIB/STM32F1/STM32F103ZET6/34_U8G2/BSP/U8G2/u8g2_test.h
@@ -0,0 +1,22 @@
+#ifndef _U8G2_TEST__H_
+#define _U8G2_TEST__H_
+
+#include "stdio.h"
+#include "delay.h"
+#include "u8g2.h"
+ 
+void testDrawProcess(u8g2_t *u8g2);
+void testShowFont(u8g2_t *u8g2);
+void testDrawFrame(u8g2_t *u8g2);
+void testDrawRBox(u8g2_t *u8g2);
+void testDrawCircle(u8g2_t *u8g2);
+void testDrawFilledEllipse(u8g2_t *u8g2);
+void testDrawMulti(u8g2_t *u8g2);
+void testDrawXBM(u8g2_t *u8g2);
+ 
+void u8g2DrawTest(u8g2_t *u8g2);
+ 
+#endif
+
+
+
